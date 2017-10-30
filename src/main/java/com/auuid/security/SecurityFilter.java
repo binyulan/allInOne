b7683000e8aa2ac2858e7935035b577044a16cde
bhan1 <Brady.Han@activenetwork.com>
2017-10-30 17:18:52 +0800

    get webApplicationContext

--- src/main/java/com/auuid/security/SecurityFilter.java
+++ src/main/java/com/auuid/security/SecurityFilter.java
@@ -1,6 +1,7 @@
 package com.auuid.security;
 
 import java.io.IOException;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -8,40 +9,30 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
 /**
  * Servlet Filter implementation class SecurityFilter
  */
 public class SecurityFilter implements Filter {
 
-    /**
-     * Default constructor. 
-     */
-    public SecurityFilter() {
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see Filter#destroy()
-	 */
-	public void destroy() {
-		// TODO Auto-generated method stub
-	}
-
+	private WebApplicationContext wac;
 	/**
 	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
 	 */
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		// TODO Auto-generated method stub
-		// place your code here
-
-		// pass the request along the filter chain
+		
 		chain.doFilter(request, response);
 	}
 
-	/**
-	 * @see Filter#init(FilterConfig)
-	 */
-	public void init(FilterConfig fConfig) throws ServletException {
+	@Override
+	public void init(FilterConfig fConfig) throws ServletException { 
+		this.wac = WebApplicationContextUtils .getWebApplicationContext(fConfig.getServletContext());
+	}
+
+	@Override
+	public void destroy() {
 		// TODO Auto-generated method stub
 	}
 
